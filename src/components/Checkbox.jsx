3ea7f685c2db571cd6df154049ce0d9e7be75267
Nilesh Sharma <nileshnilesharma@gmail.com>
2022-04-12 19:52:05 +0530

    Updated protocol skeleton

--- src/components/Checkbox.jsx
+++ src/components/Checkbox.jsx
@@ -10,18 +10,45 @@ function Checkbox() {
     setProtocols(staticProtocols)
   },[])
 
+  const handleChange= (e) => {
+    const {name, checked} = e.target
+    if(name==="selectAll"){
+      let tempProtocols = protocols.map((protocol) => {
+        return{...protocol, isChecked:checked}
+      })
+      setProtocols(tempProtocols)
+    }
+    else {
+      let tempProtocols = protocols.map(protocol => protocol.name===name ? {...protocol, isChecked: checked}:protocol)
+      setProtocols(tempProtocols)
+    }
+    // console.log(`updating with flag ${protocols}`)
+  }
+
   return (
     <div className='checkbox-container' >
       <form className='form'>
         <h1>Protocols</h1>
         <div className="checkoption-container">
-          <input type="checkbox" className='checkbox-option'/>
-          <label className="form-check-label">Select All</label>
+          <input 
+            type="checkbox" 
+            className='checkbox-option'
+            name="selectAll"
+            checked = {protocols.filter((protocol)=> protocol?.isChecked!==true).length<1}
+            onChange={handleChange}/>
+          <label className="form-check-label">
+              Select All
+          </label>
         </div>
 
         {protocols.map((protocol)=>(
           <div className="checkoption-container">
-            <input type="checkbox" className='checkbox-option'/>
+            <input type="checkbox" 
+              className='checkbox-option'
+              key={protocol.name}
+              name={protocol.name}
+              checked ={protocol?.isChecked ||false}
+              onChange={handleChange} />
             <label className="form-check-label">{protocol.name}</label>
         </div>
         ))}
